f98eea116dfb3ea3fb08e087b402c0c4e693654f
mwerumuchai <mwerumuchai@gmail.com>
2017-10-04 15:35:28 +0300

    fix bug

--- app/components/new-review/component.js
+++ app/components/new-review/component.js
@@ -2,11 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    saveAnswer(params) {
+    saveAnswer() {
     var params = {
       userName: this.get("userName"),
-      comment: this.get("comment")
-      }
+      comment: this.get("comment"),
+      legend: this.get("legend")
+    };
+    this.set('userName', '');
+    this.set('comment', '');
+    this.sendAction('saveAnswer', params);
     }
   }
 });
