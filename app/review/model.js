57812f79c9fb76d92150c3e01fb9a5ce55532284
mwerumuchai <mwerumuchai@gmail.com>
2017-10-04 15:34:51 +0300

    check firebase

--- app/review/model.js
+++ app/review/model.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     userName: DS.attr(),
     comment: DS.attr(),
-    legend: DS.belongsTo('legend', {async:true})
+    legend: DS.belongsTo('legend', { async: true })
 });
