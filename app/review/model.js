e20ea6e0f02f20a46185b0a8c4058fc68bbe6626
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-10-05 12:33:56 +0300

    Create Timestamp Create Timestamp to show the time a review was made by a user

--- app/review/model.js
+++ app/review/model.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     userName: DS.attr(),
     comment: DS.attr(),
-    legend: DS.belongsTo('legend', { async: true })
+    legend: DS.belongsTo('legend', { async: true }),
+    createdAt: DS.attr('date')
 });
+
